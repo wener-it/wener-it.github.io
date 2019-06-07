3bd8e0153cde48806d5ff7a2f90dbe74c0603290
wener-it <wener-it@users.noreply.github.com>
2019-06-07 13:44:48 +0200

    Add files via upload

---
+++ -/beratung-und-softwareentwicklung.vue
@@ -0,0 +1,70 @@
+﻿<template lang="md">
+
+<img class="avatar " src="https://www.gravatar.com/avatar/b9237e76b137ffa42c625cfb4e76c504?s=200" alt="Portrait Thomas Wener" />
+
+
+Thomas Wener
+============
+*IT&#8209;Beratung* und *Softwareentwicklung*
+----------------------------------------------
+
+### Eckdaten
+**Ort:** 
+: Metropolregion Frankfurt/Rhein-Main, Deutschland.<!--on&#8209;site&nbsp;und&nbsp;remote.-->
+
+
+
+*Verfügbarkeit:*
+: ab **Juli 2019**, bis zu 5 Tage/Woche 
+
+*Kontakt:*
+: ✉&nbsp;<span class="protect">E-Mail-Adresse spam-geschützt: die Minus-Zeichen und Sternchen bitte weglassen und den Bereich zwischen "--" rückwärts lesen:   -- </span><a href="#" class="crypted"
+   data-name="lto"
+   data-domain="thomas"
+   data-tld="wene"
+   onclick="window.location.href = 'mai'+this.dataset.name+':' + this.dataset.domain + '@' + this.dataset.tld + 'r.it'; return false;">renew<span class="protect"> ** </span>@<span class="protect"> ** </span>samoht</a><span class="protect"> -- </span>.it oder [XING](https://www.xing.com/profile/Thomas_Wener) <!--oder [LinkedIn](https://www.linkedin.com/in/thomas-wener/) -->
+
+
+
+### Technologien und Tools
+
+**Sprachen:**
+: 	* Java 
+	* Javascript  	
+	
+*Frontend:*
+:  	* HTML5 
+	* CSS3
+	* jQuery
+	
+*Backend:*
+:   * Coremedia WCM
+	* OpenCMS
+	* Sophora
+
+*Agile Methoden*
+: 	* Scrum Developer
+	* Clean Code
+	* Refactoring
+	* Pair Programming
+	* CI 
+	* Requirements Engineering
+	* TDD
+	* Scrum Master
+	
+*Sonstiges:*
+: 	* Git 
+	* SVN
+	* Jenkins
+	* Docker 
+	* Checkstyle
+	* EasyMock
+	* xUnit
+	* maven 
+	* ant 
+	* Sonar
+	* Spring
+  
+
+</template>
+
